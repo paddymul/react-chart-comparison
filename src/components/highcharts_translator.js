dae7d8b12a820436bb375f8b8522706a2102c4fb
Paddy Mullen <paddy@paddymullen.com>
2019-03-06 13:09:59 -0700

    working hardcoded highchats component

---
+++ src/components/highcharts_translator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { connect } from 'react-redux';
+import {plotTypes} from "../reducers";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        data: [1, 2, 3]
+    }]
+}
+
+const _HighchartsTranslator = () => <div>
+         <HighchartsReact
+             highcharts={Highcharts}
+             options={options}/>
+    </div>
+
+export const HighchartsTranslator = _HighchartsTranslator;
+/*
+const _PlotlyTranslator = ({ seriesArr }) => {
+    return (<div>
+                <Plot data={seriesArr} />
+            </div>);};
+
+const _expandSeries =  (ser, state) => {
+    // this combines the shorthand fields with their expanded versions
+    // shorthand fields include d and pwTyp
+    //
+    // eventually this function will translate between different plotting library
+    // conventions
+
+    // I'm not sure if this function should be memoized to return the
+    // same object for the same input so that unecessary re renderings
+    // aren't triggered
+    return {...ser, ...state.data[ser.d], ...plotTypes[ser.pwTyp]};};
+
+const mapStateToProps = state => ({
+    seriesArr: Object.values(state.series).map(ser => _expandSeries(ser, state))})
+
+const mapDispatchToProps = dispatch => ({
+  toggleVis: id => dispatch(toggleVisibility(id) )})
+
+export constPlotlyTranslator = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_PlotlyTranslator);
+*/
--- src/components/plot_wrapper.js
+++ src/components/plot_wrapper.js
@@ -3,13 +3,15 @@ import { connect } from 'react-redux';
 import { toggleVisibility } from '../actions';
 import PlotProps from "../components/plot_props";
 import {PlotlyTranslator} from "../components/plotly_translator";
+import {HighchartsTranslator} from "../components/highcharts_translator";
 
 
 //<Plot data={seriesArr} />
+//<PlotlyTranslator />
 const _PlotWrapper = ({ seriesArr }) => {
     return (<div>
                 <PlotProps />
-                <PlotlyTranslator />
+                <HighchartsTranslator />
                 
             </div>);};
 
